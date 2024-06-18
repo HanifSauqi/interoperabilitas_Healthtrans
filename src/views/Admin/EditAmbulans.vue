107e1ab1df51263736eed481cc6331eaac1c625f
TriRambuNug <trirambunugrohoprasetyo@mail.ugm.ac.id>
2024-06-18 15:26:15 +0700

    perubahan pada edit ambulans

--- src/views/Admin/EditAmbulans.vue
+++ src/views/Admin/EditAmbulans.vue
@@ -13,12 +13,12 @@
           </label>
           <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="plat_nomor" type="text" v-model="form.plat_nomor" required>
         </div>
-        <div class="mb-4">
+        <!-- <div class="mb-4">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="tipe">
             Tipe
           </label>
           <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="tipe" type="text" v-model="form.tipe" required>
-        </div>
+        </div> -->
         <div class="mb-6">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="status">
             Status
